5bfe166c2ebb9ae0968135d499e55c696928b5c2
Wei Zhou <weizhou@apache.org>
2026-04-07 21:48:38 +0200

    Network Namespace: support VPC

--- Network-Namespace/network-namespace.sh
+++ Network-Namespace/network-namespace.sh
@@ -152,6 +152,10 @@ EXTENSION_DETAILS="{}"
 NETWORK_ID=""
 CURRENT_DETAILS="{}"
 VPC_ID=""
+VM_DATA_FILE=""
+FW_RULES_FILE=""
+RESTORE_DATA_FILE=""
+ACL_RULES_FILE=""
 FORWARD_ARGS=()
 
 while [ $# -gt 0 ]; do
@@ -172,6 +176,18 @@ while [ $# -gt 0 ]; do
             shift 2 ;;
         --current-details)
             CURRENT_DETAILS="${2:-{}}"
+            shift 2 ;;
+        --vm-data-file)
+            VM_DATA_FILE="${2:-}"
+            shift 2 ;;
+        --fw-rules-file)
+            FW_RULES_FILE="${2:-}"
+            shift 2 ;;
+        --acl-rules-file)
+            ACL_RULES_FILE="${2:-}"
+            shift 2 ;;
+        --restore-data-file)
+            RESTORE_DATA_FILE="${2:-}"
             shift 2 ;;
         *)
             FORWARD_ARGS+=("$1")
@@ -280,12 +296,28 @@ ssh_exec() {
     fi
 }
 
+upload_file_to_remote() {
+    local host="$1" local_file="$2" tag="$3"
+    [ -f "${local_file}" ] || die "Missing local payload file: ${local_file}" 1
+
+    local remote_tmp
+    remote_tmp=$(ssh_exec "${host}" "mktemp /tmp/cs-extnet-${tag}-XXXXXX") || \
+        die "Failed to create remote temp file for ${tag}" 2
+    remote_tmp=$(printf '%s' "${remote_tmp}" | tr -d '\r\n')
+    [ -n "${remote_tmp}" ] || die "Failed to resolve remote temp file for ${tag}" 2
+
+    cat "${local_file}" | ssh_exec "${host}" "cat > '${remote_tmp}' && chmod 600 '${remote_tmp}'" || \
+        die "Failed to upload payload file for ${tag}" 2
+
+    printf '%s' "${remote_tmp}"
+}
+
 # ---------------------------------------------------------------------------
 # ensure-network-device
 # ---------------------------------------------------------------------------
 
 if [ "${COMMAND}" = "ensure-network-device" ]; then
-    [ -z "${NETWORK_ID}" ] && die "ensure-network-device: missing --network-id" 1
+    [ -z "${NETWORK_ID}" ] && [ -z "${VPC_ID}" ] && die "ensure-network-device: missing --network-id or --vpc-id" 1
 
     if [ ${#HOST_LIST[@]} -eq 0 ]; then
         die "ensure-network-device: no hosts configured. Set 'hosts' in registerExtension details." 1
@@ -311,7 +343,7 @@ if [ "${COMMAND}" = "ensure-network-device" ]; then
             h="${h// /}"
             if [ "${h}" = "${CURRENT_HOST}" ]; then
                 if host_reachable "${CURRENT_HOST}"; then
-                    log "ensure-network-device: network=${NETWORK_ID} keeping current host=${CURRENT_HOST}"
+                    log "ensure-network-device: ${NETWORK_ID:+network=${NETWORK_ID} }${VPC_ID:+vpc=${VPC_ID} }keeping current host=${CURRENT_HOST}"
                     if [ -n "${VPC_ID}" ]; then
                         printf '{"host":"%s","namespace":"%s","vpc_id":"%s"}\n' \
                             "${CURRENT_HOST}" "${NAMESPACE}" "${VPC_ID}"
@@ -350,7 +382,7 @@ if [ "${COMMAND}" = "ensure-network-device" ]; then
         _H="${HOST_LIST[$_IDX]// /}"
         if host_reachable "${_H}"; then
             _SELECTED_HOST="${_H}"
-            log "ensure-network-device: network=${NETWORK_ID} hash-selected host=${_SELECTED_HOST} (key=${_ROUTE_KEY}, idx=${_IDX})"
+            log "ensure-network-device: ${NETWORK_ID:+network=${NETWORK_ID} }${VPC_ID:+vpc=${VPC_ID} }hash-selected host=${_SELECTED_HOST} (key=${_ROUTE_KEY}, idx=${_IDX})"
             break
         fi
         log "ensure-network-device: host ${_H} not reachable, trying next"
@@ -386,6 +418,28 @@ for arg in "${FORWARD_ARGS[@]}"; do
     remote_args+=("'${arg//"'"/"'\\''"}'" )
 done
 
+REMOTE_PAYLOAD_FILES=()
+if [ -n "${VM_DATA_FILE}" ]; then
+    REMOTE_VM_DATA_FILE=$(upload_file_to_remote "${REMOTE_HOST}" "${VM_DATA_FILE}" "vm-data")
+    REMOTE_PAYLOAD_FILES+=("${REMOTE_VM_DATA_FILE}")
+    remote_args+=("'--vm-data-file'" "'${REMOTE_VM_DATA_FILE//"'"/"'\\''"}'")
+fi
+if [ -n "${FW_RULES_FILE}" ]; then
+    REMOTE_FW_RULES_FILE=$(upload_file_to_remote "${REMOTE_HOST}" "${FW_RULES_FILE}" "fw-rules")
+    REMOTE_PAYLOAD_FILES+=("${REMOTE_FW_RULES_FILE}")
+    remote_args+=("'--fw-rules-file'" "'${REMOTE_FW_RULES_FILE//"'"/"'\\''"}'")
+fi
+if [ -n "${ACL_RULES_FILE}" ]; then
+    REMOTE_ACL_RULES_FILE=$(upload_file_to_remote "${REMOTE_HOST}" "${ACL_RULES_FILE}" "acl-rules")
+    REMOTE_PAYLOAD_FILES+=("${REMOTE_ACL_RULES_FILE}")
+    remote_args+=("'--acl-rules-file'" "'${REMOTE_ACL_RULES_FILE//"'"/"'\\''"}'")
+fi
+if [ -n "${RESTORE_DATA_FILE}" ]; then
+    REMOTE_RESTORE_DATA_FILE=$(upload_file_to_remote "${REMOTE_HOST}" "${RESTORE_DATA_FILE}" "restore-data")
+    REMOTE_PAYLOAD_FILES+=("${REMOTE_RESTORE_DATA_FILE}")
+    remote_args+=("'--restore-data-file'" "'${REMOTE_RESTORE_DATA_FILE//"'"/"'\\''"}'")
+fi
+
 PHYS_ESCAPED="${PHYS_DETAILS//\'/\'\\\'\'}"
 EXT_ESCAPED="${EXTENSION_DETAILS//\'/\'\\\'\'}"
 REMOTE_CMD="'${REMOTE_SCRIPT}' '${COMMAND}' ${remote_args[*]} --physical-network-extension-details '${PHYS_ESCAPED}' --network-extension-details '${EXT_ESCAPED}'"
@@ -394,6 +448,12 @@ log "Remote: ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PORT} cmd=${COMMAND}"
 
 RC=0
 ssh_exec "${REMOTE_HOST}" "${REMOTE_CMD}" || RC=$?
+
+if [ ${#REMOTE_PAYLOAD_FILES[@]} -gt 0 ]; then
+    for _rf in "${REMOTE_PAYLOAD_FILES[@]}"; do
+        ssh_exec "${REMOTE_HOST}" "rm -f '${_rf}'" >/dev/null 2>&1 || true
+    done
+fi
 
 if [ ${RC} -ne 0 ]; then
     if [ ${RC} -eq 255 ]; then
